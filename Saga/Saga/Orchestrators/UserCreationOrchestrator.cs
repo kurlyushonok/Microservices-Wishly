88e87040e37d639ccb417c38218c19b8366d89b3
kurlyushonok <history05mikano@gmail.com>
2025-10-28 22:50:06 +0500

    orchestrator realisation

--- Saga/Saga/Orchestrators/UserCreationOrchestrator.cs
+++ Saga/Saga/Orchestrators/UserCreationOrchestrator.cs
@@ -1,6 +1,15 @@
-﻿namespace Saga.Orchestrators;
+﻿using MassTransit;
 
-public class UserCreationOrchestrator
+namespace Saga.Orchestrators;
+
+public class UserCreationOrchestrator : SagaStateMachineInstance
 {
+    public Guid CorrelationId { get; set; }
+    public string CurrentState { get; set; } = "Initial";
     
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public Guid? UserId { get; set; }
+    public Guid? WishlistId { get; set; }
+    public string? FailureReason { get; set; }
 }
