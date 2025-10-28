76b3493fb8e1494f962b9f919f365e54f839ee50
kurlyushonok <history05mikano@gmail.com>
2025-10-28 23:51:01 +0500

    create coordinator

---
+++ Saga/Saga/Coordinators/UserCreationCoordinator.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+using Saga.Contracts;
+
+namespace Saga.Coordinators;
+
+public class UserCreationCoordinator : IConsumer<StartUserCreation>
+{
+    public async Task Consume(ConsumeContext<StartUserCreation> context)
+    {
+        try
+        {
+            await context.Publish(context.Message);
+        }
+        catch (Exception ex)
+        {
+            await context.Publish(new UserCreationFailed
+            {
+                CorrelationId = context.Message.CorrelationId,
+                Reason = $"Failed to start saga: {ex.Message}"
+            });
+        }
+    }
+}
