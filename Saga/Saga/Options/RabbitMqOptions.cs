5eb6ace91387f6932fc798b45adb3a7e31109ffe
kurlyushonok <history05mikano@gmail.com>
2025-10-29 23:06:39 +0500

    config saga2

---
+++ Saga/Saga/Options/RabbitMqOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Saga.Options;
+
+public class RabbitMqOptions
+{
+    public string Host { get; set; } = "localhost";
+    public string Username { get; set; } = "guest";
+    public string Password { get; set; } = "guest";
+    public string VirtualHost { get; set; } = "/";
+}
---
+++ Saga/Saga/SagaDbContext.cs
@@ -0,0 +1,44 @@
+﻿using MassTransit;
+using MassTransit.EntityFrameworkCoreIntegration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Saga.Orchestrators;
+
+namespace Saga;
+
+public class DbContext : SagaDbContext
+{
+    public DbContext(DbContextOptions<SagaDbContext> options) 
+        : base(options)
+    {
+    }
+
+    protected override IEnumerable<ISagaClassMap> Configurations
+    {
+        get { yield return new UserCreationOrchestratorMap(); }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasDefaultSchema("saga");
+    }
+}
+
+public class UserCreationOrchestratorMap : SagaClassMap<UserCreationOrchestrator>
+{
+    protected override void Configure(EntityTypeBuilder<UserCreationOrchestrator> entity, ModelBuilder model)
+    {
+        entity.ToTable("user_creation_sagas");
+        entity.Property(x => x.Username)
+            .HasMaxLength(100)
+            .IsRequired();
+        entity.Property(x => x.PasswordHash)
+            .HasMaxLength(255);
+        entity.Property(x => x.FailureReason)
+            .HasMaxLength(1000);
+        
+        entity.HasIndex(x => x.Username);
+        entity.HasIndex(x => x.CurrentState);
+    }
+}
