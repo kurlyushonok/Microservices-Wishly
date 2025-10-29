1e6a4e0eff7348b96a17c5a9b68ba8bdeba36079
kurlyushonok <history05mikano@gmail.com>
2025-10-29 23:02:49 +0500

    config saga

--- Saga/Saga/Program.cs
+++ Saga/Saga/Program.cs
@@ -1,7 +1,56 @@
-using Saga;
+using MassTransit;
+using MassTransit.EntityFrameworkCoreIntegration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Saga.Orchestrators;
+using Saga.Coordinators;
+using Saga.Options;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services.Configure<RabbitMqOptions>(
+    builder.Configuration.GetSection("RabbitMq"));
+
+builder.Services.AddDbContext<SagaDbContext>((provider, options) =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("SagaDatabase");
+    
+    options.UseNpgsql(connectionString);
+});
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddSagaStateMachine<UserCreationOrchestratorStateMachine, UserCreationOrchestrator>()
+        .EntityFrameworkRepository(r =>
+        {
+            r.ConcurrencyMode = ConcurrencyMode.Pessimistic;
+            r.ExistingDbContext<SagaDbContext>();
+        });
+    
+    x.AddConsumer<UserCreationCoordinator>();
+    
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        var options = context.GetRequiredService<IOptions<RabbitMqOptions>>().Value;
+        
+        cfg.Host(options.Host, "/", h =>
+        {
+            h.Username(options.Username);
+            h.Password(options.Password);
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+builder.Services.AddHealthChecks();
 
 var host = builder.Build();
-host.Run();
+
+using (var scope = host.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<SagaDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
+
+await host.RunAsync();
